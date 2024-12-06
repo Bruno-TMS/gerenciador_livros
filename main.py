58a26b54c77b9f8f43d5f4e3438ade8de3c5d460
Mendes <btmsba@outlook.com>
2024-12-06 17:50:07 -0300

    ajustanto main

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ import ordenamento as ordmt
 if __name__ == "__main__":
     ordmt.criar_pasta_originais()
     ordmt.criar_pasta_producao()
-    ordmt.criar_pasta_producao()
     ordmt.copiar_arquivos()
