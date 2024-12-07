47e67879c5507e71b6918cb886598a0f6cbedac4
Mendes <btmsba@outlook.com>
2024-12-07 08:46:40 -0300

    criado pre-ordenamento

--- ordenamento.py
+++ ordenamento.py
@@ -2,10 +2,14 @@
 import shutil
 from pathlib import Path
 import re
-import unicodedata 
+import unicodedata
+from docx import Document
+import csv
+from pprint import pprint as pp
 
 ORIGINAIS_PATH = Path('originais')
 PRODUCAO_PATH = Path('producao')
+
 
 def criar_pasta_originais():
     try:
@@ -45,6 +49,7 @@ def copiar_arquivos():
         shutil.copy(arquivo, caminho_destino)
         print(f"Arquivo {arquivo.name} copiado e renomeado para {nome_renomeado}")
 
+
 def renomear_arquivo(nome):
     # Separa o nome do arquivo da sua extensão
     nome_base, extensao = nome.rsplit('.', 1)
@@ -68,9 +73,47 @@ def renomear_arquivo(nome):
     # Junta novamente o nome com a extensão
     return f"{nome_base}.{extensao}"
 
+
 def listar_arquivos():
     for x in ORIGINAIS_PATH.glob('*.docx'):
         print (x.name)
 
-if __name__ == "__main__":
-    pass
+
+def criar_preordenamento():
+    'método que vai extrair path de origem, titulo, autor e inserir uma ordem em um csv'
+    
+    if not PRODUCAO_PATH.exists():
+        raise FileNotFoundError('Pasta de produção não existe')
+    
+    arquivos = {k:{'path': v} for k, v in enumerate(sorted(PRODUCAO_PATH.glob('*.docx')))}
+        
+    nome_restricao_letras = 250
+        
+    for k, v in arquivos.items():
+        wd = Document(v['path'])
+        wd_prgs = wd.paragraphs
+            
+        prgs = [x for x in wd_prgs if x.text.strip()]
+            
+        v.setdefault('titulo', prgs[0].text.strip().rstrip(',.'))
+            
+        if len(prgs[1].text.strip()) <= nome_restricao_letras:
+            v.setdefault('autor', prgs[1].text.strip().rstrip(',.'))
+        else:
+            v.setdefault('autor', '')
+            
+        campos = ['ordem', 'path', 'titulo', 'autor']
+        
+        with open(Path(PRODUCAO_PATH,'00_pre_ordenamento_automatico'), mode='w', newline='', encoding='utf-8') as arquivo_csv:
+            escritor_csv = csv.DictWriter(arquivo_csv, fieldnames=campos, delimiter=';')
+            
+            for ordem, info in sorted(arquivos.items()):
+                escritor_csv.writerow({
+                    'ordem': ordem + 1
+                    , 'path': str(Path(info.get('path', '')).name)
+                    , 'titulo': info.get('titulo', '')
+                    , 'autor': info.get('autor', '')
+                    
+            })
+                
+        pp(arquivos)
